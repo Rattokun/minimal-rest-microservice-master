8f200ce21b0aa907edb7364a3378223a2dd3a454
Rattokun <vitaliy-kuzin-00@mail.ru>
2024-03-17 16:20:11 +0300

    new commit

---
+++ securities-microservice/main.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+import random
+import uvicorn
+import os
+
+app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    r_id = random.randint(298, 10000)
+    r = "100"
+    return r.json()
+
+
+@app.get("/list/")
+async def get_list(q: list | None = Query()):
+    film_list = []
+    for id in q:
+        r = requests.get(f"https://kinopoiskapiunofficial.tech/api/v2.2/films/{id}",
+                         headers={'X-API-KEY': '700211e1-f970-499f-9957-6bca24e2adb1'})
+        film_list.append(r.json())
+    return film_list
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="0.0.0.0", port=int(os.getenv('PORT', 80)))
